3d3adcb74175d0b8608c814b4ed1310d98fbbb3c
Bastien Perrot <bastien.perrot1@gmail.com>
2020-10-14 16:02:57 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,12 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters(string) {
+    return string
+      .split(' ')
+      .map(word => word.substr(0,1).toUpperCase() + word.substr(1,word.length))
+      .join(' ');
+  };
+
 // TEST
         assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
    
